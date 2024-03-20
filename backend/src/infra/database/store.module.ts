4f0b39465ccd01cedf45d3eb600c3c9cdf9d90b3
mauricio.costa@yduqs.com.br <mauricio.costa@yduqs.com.br>
2024-03-20 16:37:44 -0300

    \o/\o/\o/\o/

--- backend/src/infra/database/store.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { StoreProviders } from "./store.provider";
-
-@Module({
-    providers: [...StoreProviders],
-    exports: [...StoreProviders]
-})
-export class StoreModule {}
