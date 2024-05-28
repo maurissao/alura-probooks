a371d9993b391564d430a551131a0cbcc579c308
mauricio.costa@yduqs.com.br <mauricio.costa@yduqs.com.br>
2024-05-27 21:22:03 -0300

    Semana9

---
+++ frontend/src/componentes/NavBar/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import estilos from './NavBar.module.scss';
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <header className={estilos.menu_bg}>
+        <div className={estilos.menu}>
+            <div className={estilos.menu_logo}>
+                <Link to="/">
+                    <i className="pb-logo fa-sharp fa-solid fa-book-open "></i>
+                    <span>ProBooks</span>
+                </Link>
+            </div>
+            <nav className={estilos.menu_nav}>
+                <ul>
+                    <li><Link to="#sobre">Sobre</Link></li>
+                    <li><Link to="#produtos">Produtos</Link></li>
+                    <Link to="#carrinho" className={estilos.carrinho_home}>
+                        <i className="fa-solid fa-cart-shopping fa-lg"></i>
+                        <div className={estilos.cart_count_circle}>
+                            <span className={estilos.cart_count}>0</span>
+                        </div>
+                    </Link>
+                </ul>
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default NavBar
---
+++ frontend/src/paginas/home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import NavBar from '../../componentes/NavBar';
+import estilos from './home.module.scss';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
